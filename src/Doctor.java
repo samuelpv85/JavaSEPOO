307d34af4a37aa2990e84ccb5726be20f7cbcf70
Samuel Patiño <levivo2@hotmail.com>
2023-03-27 23:39:26 -0400

    Primera clase con modularidad

---
+++ src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+//    atributos
+    int id;
+    String name;
+    String Speciality;
+
+//    Comportamiento - Metodo
+    public void showName(){ //no devuelve nada
+        System.out.println("Mi Nombre : " + name);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+//    instanciar la clase Doctor;
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Samuel";
+        myDoctor.showName();
+    }
+}
